5b7f5a784483c05a08de222a64e29fef4032150a
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-30 14:17:29 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class song 
+
+  attr_accessor :name, :artist, :genre
+
+  @@count=0
+
+  def initialize
+    count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def count=(count)
+    @count=count
+  end
+
+  def count
+    @count
+  end
+
+end
