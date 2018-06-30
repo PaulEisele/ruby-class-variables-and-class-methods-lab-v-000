43e43ef15ba3da12de82c7dcc7406b4a4bb105ac
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-30 14:37:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,10 +24,24 @@ class Song
     @@genres.each{|genre| genre_list << genre unless genre_list.include?(genre)}
     genre_list
   end
-  
+
   def self.artists
     artist_list=[]
     @@artists.each{|artist| artist_list << artist unless artist_list.include?(artist)}
     artist_list
+  end
+
+  def self.artist_count
+    @@artist_count.each do |artist|
+      @@artist_count[artist] = @@artist.count(artist)
+    end
+    @@artist_count
+  end
+
+  def self.genre_count
+    @@genre_count.each do |genre|
+      @@genre_count[genre] = genre.count(genre)
+    end
+    @@genre_count
   end
 end
