8dadcc3d216908646d910f8fa0638c4139662406
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-30 14:20:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,8 @@ class song
   attr_accessor :name, :artist, :genre
 
   @@count=0
+  @@genre=[]
+  @@artist=[]
 
   def initialize
     count += 1
@@ -13,7 +15,7 @@ class song
   end
 
   def count=(count)
-    @count=count
+    @count = count
   end
 
   def count
